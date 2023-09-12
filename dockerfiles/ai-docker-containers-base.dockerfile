d2ae06ad38c3a2b8ae2db3f4b208087eec801f99
Irving A. Bermúdez S <bigchirv@gmail.com>
2023-09-12 14:47:46 -0400

    Changes

--- dockerfiles/ai-docker-containers-base.dockerfile
+++ dockerfiles/ai-docker-containers-base.dockerfile
@@ -1,4 +1,33 @@
-FROM chirvo/pytorch-rocm-jammy:latest
+FROM ubuntu:jammy
+
+ENV LC_ALL=C.UTF-8 LANG=C.UTF-8 USE_CUDA=0
+#################
+# Common envvars
+# Try running it with this command if you have issues:
+# For 6700, 6600 and maybe other RDNA2 or older:
+ENV HSA_OVERRIDE_GFX_VERSION="10.3.0"
+#ENV PYTORCH_ROCM_ARCH="gfx1030"
+
+# For AMD 7600 and maybe other RDNA3 cards: 
+#ENV HSA_OVERRIDE_GFX_VERSION="11.0.0"
+#ENV PYTORCH_ROCM_ARCH="gfx1100"
+
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get -y dist-upgrade \
+    && apt-get install -y \
+    apt-utils gnupg software-properties-common wget dumb-init ffmpeg git jq liblcms2-2 libz3-4 \
+    libtcmalloc-minimal4 pkg-config python3-pip python3-venv \
+    && apt-get clean \
+    && wget https://repo.radeon.com/amdgpu-install/5.6/ubuntu/jammy/amdgpu-install_5.6.50600-1_all.deb \
+    && apt-get install -y ./amdgpu-install_5.6.50600-1_all.deb \
+    && amdgpu-install -y --no-dkms --usecase=rocm,hip,mllib --no-32 \
+    && rm ./amdgpu-install_5.6.50600-1_all.deb \
+    && apt-get update && apt-get -y dist-upgrade && apt-get install -y \
+    hip-rocclr llvm-amdgpu \
+    && apt-get clean \
+    && pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm5.6
 
 WORKDIR /
 COPY scripts/entrypoint.sh .
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
  
 #################
-# Common envvars
-# Try running it with this command if you have issues:
-# For 6700, 6600 and maybe other RDNA2 or older:
-export HSA_OVERRIDE_GFX_VERSION="10.3.0"
-#export PYTORCH_ROCM_ARCH="gfx1030"
-
-# For AMD 7600 and maybe other RDNA3 cards: 
-#export HSA_OVERRIDE_GFX_VERSION="11.0.0"
-#export PYTORCH_ROCM_ARCH="gfx1100"
-
-#################
 # Script globals
 GIT_URI="https://github.com/AUTOMATIC1111/stable-diffusion-webui"
 COMMAND_EXTRA_PARAMS=""
@@ -19,14 +8,19 @@ COMMAND="python3 launch.py --listen --enable-insecure-extension-access"
 PORT=7860
 # generated envvars
 
+_run_or_die () {
+  $*
+  if [ $? -ne 0 ]; then
+    echo "Error: command -= $* =- failed"
+    exit 1
+  fi
+}
 
 _get_dest_dir () {
-
   local BASE_DIR=$(pwd)
   if [ "$BASE_DIR" == "/" ]; then
     BASE_DIR=""
   fi
-
   # returns DEST_DIR
   echo "$BASE_DIR/$(basename $GIT_URI .git)"
 }
@@ -34,12 +28,12 @@ _get_dest_dir () {
 _clone_or_rebase_repo () {
   local DEST_DIR=$(_get_dest_dir)
   if [ -d "$DEST_DIR" ]; then
-    echo "Directory $DEST_DIR exists. Rebasing."
-    cd $DEST_DIR
-    git rebase
-    cd -
+    echo "Directory $DEST_DIR exists. Pulling changes."
+    _run_or_die cd $DEST_DIR
+    _run_or_die git pull
+    _run_or_die cd -
   else
-    git clone $GIT_URI $DEST_DIR
+    _run_or_die git clone $GIT_URI $DEST_DIR
   fi
 
   cd $DEST_DIR
@@ -47,20 +41,32 @@ _clone_or_rebase_repo () {
   cd -
 }
 
+_prepare_venv () {
+  _run_or_die python3 -m venv venv --system-site-packages
+  source ./venv/bin/activate
+}
+
 _install_requirements () {
   local DEST_DIR=$(_get_dest_dir)
-  cd $DEST_DIR
-  cat requirements.txt | grep -vw torch > /tmp/requirements.txt
-  mv /tmp/requirements.txt ./requirements.txt
-  pip3 install -r requirements.txt
+  _run_or_die cd $DEST_DIR
+  if [ ! -f "requirements.txt" ]; then
+    echo "Error: 'requirements.txt does not exist.'"
+    exit 1
+  fi
+  # cat requirements.txt | grep -vw torch > ./_requirements.txt
+  # mv ./_requirements.txt ./requirements.txt
+  _run_or_die pip3 install -r requirements.txt
   cd -
 }
 
 run () {
   local DEST_DIR=$(_get_dest_dir)
 
-  echo "1. Cloning $GIT_URI into $DEST_DIR"
+  echo "0. Cloning $GIT_URI into $DEST_DIR"
   _clone_or_rebase_repo
+
+  echo "1. Preparing venv"
+  _prepare_venv
 
   echo "2. Installing requirements"
   _install_requirements
@@ -83,7 +89,7 @@ case "$1" in
   comfy) echo "Running ComfyUI"
     GIT_URI="https://github.com/comfyanonymous/ComfyUI.git"
     COMMAND_EXTRA_PARAMS=""
-    COMMAND="python3 main.py --listen --enable-insecure-extension-access"
+    COMMAND="python3 main.py --listen --enable-insecure-extension-access --precision full --no-half --opt-sub-quad-attention"
     PORT=8188
   ;;
   bash) echo "Running /bin/bash"
