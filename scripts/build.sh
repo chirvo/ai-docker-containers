1b596b11a1cd405724d6f7f54df395e432ed0a3a
Irving A. Bermúdez S <bigchirv@gmail.com>
2024-01-04 02:14:26 -0500

    fixing build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
+IMAGES=$(echo $(basename -s .dockerfile -a $(ls ./dockerfiles/*.dockerfile) | sed -e 's/ai-//g'))
+echo "${IMAGES}"
 
-IMAGES=$(basename -s .dockerfile -a "$(ls ./dockerfiles/*.dockerfile)" | sed -e 's/ai-//')
-
-_build () {
+_build() {
   # $1: dockerfile's name
   if [ ! -f "./dockerfiles/$1.dockerfile" ]; then
     echo "Error: './dockerfiles/$1.dockerfile' does not exists. Cannot build."
     exit 1
   fi
+  echo -n "Building image '$1': "
   docker build --tag "chirvo/$1:latest" -f "./dockerfiles/$1.dockerfile" .
+  [ $? -eq 0 ] && echo "done." || echo "error."
 }
-_image_rm () {
-#$1 Image
+_image_rm() {
+  #$1 Image
   echo -n "Removing image '$1': "
   docker image rm "chirvo/$1:latest"
-  [ $? -eq 0 ] && echo "done." || echo "error." 
+  [ $? -eq 0 ] && echo "done." || echo "error."
 }
 
 case "$1" in
-clean) 
+clean)
   echo "Pruning containers..."
   docker container prune
-  for IMAGE in $IMAGES
-  do
+  for IMAGE in $IMAGES; do
     _image_rm "ai-$IMAGE"
   done
   ;;
-all) echo "Building $1"
-  for IMAGE in $IMAGES
-  do
+all)
+  echo "Building $1"
+  for IMAGE in $IMAGES; do
     _build "ai-$IMAGE"
   done
   ;;
 *)
-if [ true ]; then
-  echo "It is true"
-else
-	echo "Usage: $0 {help|clean|all}"
+  for IMAGE in $IMAGES; do
+    if [ "$1" == "$IMAGE" ]; then
+      echo "Found $1."
+      _build "ai-$1"
+      exit 0
+    fi
+  done
+  echo "Usage: $0 {help|clean|all|<image>}"
+  echo ""
+  echo "Available images: $IMAGES".
+  echo ""
   exit 1
-  fi
   ;;
 esac
 exit 0
